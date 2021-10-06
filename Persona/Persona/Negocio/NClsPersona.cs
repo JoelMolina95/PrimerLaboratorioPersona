7b1506784afce329a5ddbae956e3e19eeae5da06
JoelJR26 <91918009+JoelMolina95@users.noreply.github.com>
2021-10-05 20:45:30 -0600

    Laboratorio

---
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -0,0 +1,37 @@
+﻿using Persona.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Negocio
+{
+    class NClsPersona
+    {
+        public String Hablar(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " Está hablando ";
+
+        }
+
+        public String Caminar(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " Está caminando ";
+        }
+
+        public String Reir(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " Está ríendo ";
+        }
+
+        public String Observar(ClsPersona persona)
+        {
+
+            return " La persona de nombre " + persona.Nombre + " Está observando ";
+        }
+    }
+}
---
+++ Persona/Persona/Program.cs
@@ -0,0 +1,28 @@
+﻿using Persona.Entidades;
+using Persona.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsPersona clspersona = new ClsPersona();
+            NClsPersona nclspersona = new NClsPersona();
+
+            clspersona.Nombre = "Martin";
+
+            Console.WriteLine(nclspersona.Hablar(clspersona));
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
